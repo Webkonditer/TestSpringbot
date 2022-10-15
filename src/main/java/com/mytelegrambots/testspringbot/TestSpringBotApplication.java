ef3d20f4926a8f6976b600ae9b8b4d839e422454
Webkonditer <webkonditer@yandex.ru>
2022-10-15 23:42:00 +0300

    Бот научился отвечать на /start

---
+++ src/main/java/com/mytelegrambots/testspringbot/TestSpringBotApplication.java
@@ -0,0 +1,13 @@
+package com.mytelegrambots.testspringbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytelegrambots/testspringbot/TestSpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytelegrambots.testspringbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
